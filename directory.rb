d498105130e4952ac4eae9d14a535c67a2547c16
Dan <djclissold@yahoo.co.uk>
2016-06-21 17:11:43 +0100

    Replace chomp with rstrip

--- directory.rb
+++ directory.rb
@@ -4,7 +4,7 @@ def input_students
   #create an empty array
   students = []
   #get the first name
-  name = gets.chomp
+  name = gets.rstrip! #rstrip! used instead of .chomp exercise 8.10
 
   #while the name is not empty, repeat this code
   while !name.empty? do
